27fada5530359a5e8111d36ff746e27455e8786b
John Xu <john.xud@gmail.com>
2021-03-23 21:20:04 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# AspNetCore.Utilities.Common
+
+This repository is a collection of some common useful helper functions, extensions and packages for ASP.NET Core coding.
+
+**ASP.NET Core** versions supported: 5.0, 3.1, 3.0 and 2.2
+
+## Build
+
+### Tools
+
+- dotnet.exe command line
+- Visual Studio / Code
+
+## Usage
+
+Build the library yourself or import the NuGet package.
---
+++ src/AspNetCore.Utilities.Text/StringExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace AspNetCore.Utilities.Text
+{
+    public static class StringExtensions
+    {
+        public static string SubstringAfter(this string text, string afterText = "@")
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                int textPosition = text.LastIndexOf(afterText, StringComparison.Ordinal);
+                if (textPosition > 0)
+                {
+                    return text[(textPosition + 1)..];
+                }
+
+                return text;
+            }
+
+            return string.Empty;
+        }
+
+        public static string SubstringBefore(this string text, string beforeText = "@")
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                int textPosition = text.IndexOf(beforeText, StringComparison.Ordinal);
+                if (textPosition > 0)
+                {
+                    return text.Substring(0, textPosition);
+                }
+
+                return text;
+            }
+
+            return string.Empty;
+        }
+
+        public static string Base64Encode(this string text)
+        {
+            return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(text));
+        }
+
+        public static string RemoveInvalidXmlChars(this string text)
+        {
+            var validXmlChars = text.Where(XmlConvert.IsXmlChar).ToArray();
+            return XmlConvert.EncodeName(new string(validXmlChars));
+        }
+    }
+}
